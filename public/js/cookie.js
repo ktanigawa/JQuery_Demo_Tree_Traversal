e1d1a2dd78f09c2f0fbd823ab25dc6a7e5686c94
yukio yamamoto <aj.yamamoto@me.com>
2015-02-20 11:26:59 -1000

    done

--- public/js/cookie.js
+++ public/js/cookie.js
@@ -11,35 +11,8 @@
 //
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 $(function(){
-  var closest = $("div.closest");
+  var closest = $('body').find('.closest');
   closest.click(function() {
     $(".vi").delay(3000).append('<iframe width="640" height="385" src="//www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allowfullscreen></iframe>');
     $( ".vi" ).show( "slow" );
