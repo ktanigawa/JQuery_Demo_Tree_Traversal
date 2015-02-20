06182d737ada71ed2230a18f426ea6a411e74461
yukio yamamoto <aj.yamamoto@me.com>
2015-02-20 11:43:07 -1000

    done

--- public/js/cookie.js
+++ public/js/cookie.js
@@ -14,7 +14,9 @@
 $(function(){
   var closest = $('body').find('.closest');
   closest.click(function() {
-    $(".vi").delay(3000).append('<iframe width="640" height="385" src="//www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+    setTimeout(function() {
+    $(".vi").append('<iframe width="640" height="385" src="//www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allowfullscreen></iframe>');
     $( ".vi" ).show( "slow" );
+    },10000);
   });
 });
